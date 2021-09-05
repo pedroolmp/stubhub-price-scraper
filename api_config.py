1053a0bdefcbd2db05d8cdd01fd628c92ee03b18
pedroolmp <90154461+pedroolmp@users.noreply.github.com>
2021-09-05 12:23:47 -0700

    Update api_config.py

--- api_config.py
+++ api_config.py
@@ -1,9 +1,9 @@
 # Your stubhub username and password
-username = ''
-password = ''
+username = 'pedro.olmp@gmail.com'
+password = 'Kydland1977'
 
 # Production auth token provided by the stubhub api
-auth_token = ''
+auth_token = 'OhVHGw6T9SGBmteqUznQG7i1GuGW'
 
 #List of event ids for which you'd like to scrape price data
-event_ids = []
+event_ids = [104894857]
